84562432717865657d91fb22ead392909c8de120
Chin Lee Wong <chinlee.wong@hotmail.com>
2021-11-22 16:05:31 +0100

    Routes Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home',
+  get "home", to: "pages#home",
+  get "dashboard", to: "homepages#dashboard", as: :dashboard,
+  get "movies/id", to: "movies#show",
+  get "movies-id", to: "movies#show",
+  get "watch-parties", to: "watch-parties#show",
+  post "watch-parties/attendees", to: "attendees#create",
+  get "watch-parties/new", to: "users#new",
+  post "messages", to: "messages#create",
+  get "movie/id", to: "movies#show",
+  post "movie/id/reviews", to: "reviews#create",
+  get "home", to: "homepages#home"
 end
