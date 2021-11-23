fd657cf8f5bef794703648fc466e06091c145ad4
jf dh <jf@opencollective.brussels>
2021-11-23 11:40:56 +0100

    minor

--- app/controllers/watch_parties_controller.rb
+++ app/controllers/watch_parties_controller.rb
@@ -1,2 +1,9 @@
 class WatchPartiesController < ApplicationController
+  def index
+    @watch_parties = WatchParty.all
+  end
+
+  def show
+    @watch_parties = WatchParty.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+WatchParty.create(date: "06/12/2021", location: "Charleroi", id_movie: 1)
