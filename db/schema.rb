2d20ee42160ff7ebecfd2668dbc79cf04a56a58e
julien chapelle <julien.chapelle@gmail.com>
2021-12-01 10:51:46 +0100

    fixed navbar links

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_11_25_101712) do
-=======
 ActiveRecord::Schema.define(version: 2021_11_30_164056) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
